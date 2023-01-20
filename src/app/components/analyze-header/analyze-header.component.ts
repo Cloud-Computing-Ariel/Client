328a28cb2da7cda1554003cf48f3a1ba0427b8b4
Alex Baranov <73061540+SaltyByte@users.noreply.github.com>
2023-01-20 22:22:47 +0200

    added charts

--- src/app/components/analyze-header/analyze-header.component.ts
+++ src/app/components/analyze-header/analyze-header.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-analyze-header',
   templateUrl: './analyze-header.component.html',
-  styleUrls: ['./analyze-header.component.css']
+  styleUrls: ['./analyze-header.component.css'],
 })
-export class AnalyzeHeaderComponent {
-
-}
+export class AnalyzeHeaderComponent {}
